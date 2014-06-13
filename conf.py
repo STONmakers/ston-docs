3fe8dea2f594a8d087173b6d177a23821ef9f23e
winesoft <service1@winesoft.co.kr>
2014-06-13 20:51:17 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'STON'
-copyright = '2014, JAEHA AHN'
+copyright = '2014, WineSOFT Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
