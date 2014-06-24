09afe14827ef5b2fc4e9a6ab6f4d2f93cbdbb6f7
winesoft <service1@winesoft.co.kr>
2014-06-24 19:39:22 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -125,7 +125,7 @@ html_theme = 'default'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '/favicons/favi_ston_t.ico'
+html_favicon = 'favicons/ston_t.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
